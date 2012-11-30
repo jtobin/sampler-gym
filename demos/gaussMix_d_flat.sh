496b69f30d9b43268ebd796f41d6ebdb4bc92967
Jared Tobin <jared@jtobin.ca>
2012-11-30 23:01:47 +1300

    Tweak demo scripts.

--- demos/gaussMix_d_flat.sh
+++ demos/gaussMix_d_flat.sh
@@ -8,16 +8,18 @@
 dpObservations=100
 dpAlpha=1.0
 gridUpperBound=100
-nFlatSamples=1000
+nFlatSamples=10000
 nFlatParticles=1000
 burnIn=0
 thinEvery=250
 prngSeed=42
 genType="Dirichlet"
 
+NOW=$(date +"%Y-%m-%d-%H%M")
+
 ./GaussMix_flat \
     $dpObservations $dpAlpha $gridUpperBound $nFlatSamples $nFlatParticles $burnIn $thinEvery $prngSeed $genType \
     +RTS -N -qg -s \
-    >  ~/projects/sampler-gym/data/output/GaussianMixture/"$genType"/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed".dat \
-    2> ~/projects/sampler-gym/data/output/GaussianMixture/"$genType"/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed".rts 
+    >  ~/projects/sampler-gym/data/output/GaussianMixture/$genType/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed"_"$NOW".dat \
+    2> ~/projects/sampler-gym/data/output/GaussianMixture/$genType/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed"_"$NOW".rts 
 
--- demos/gaussMix_dp_flat.sh
+++ demos/gaussMix_dp_flat.sh
@@ -15,9 +15,11 @@ thinEvery=250
 prngSeed=42
 genType="DirichletProcess"
 
+NOW=$(date +"%Y-%m-%d-%H%M")
+
 ./GaussMix_flat \
     $dpObservations $dpAlpha $gridUpperBound $nFlatSamples $nFlatParticles $burnIn $thinEvery $prngSeed $genType \
     +RTS -N -qg -s \
-    >  ~/projects/sampler-gym/data/output/GaussianMixture/"$genType"/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed".dat \
-    2> ~/projects/sampler-gym/data/output/GaussianMixture/"$genType"/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed".rts 
+    >  ~/projects/sampler-gym/data/output/GaussianMixture/"$genType"/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed"_"$NOW".dat \
+    2> ~/projects/sampler-gym/data/output/GaussianMixture/"$genType"/gaussMix_"$gridUpperBound"_"$nFlatSamples"_"$nFlatParticles"_"$burnIn"_"$thinEvery"_"$prngSeed"_"$NOW".rts 
 
